a510b1e09b6a51f9177b81b2468191dcfd7a3e70
DoheumPark <pirbak@hanmail.net>
2024-10-16 17:22:22 +0900

    10-16 (오후)(3)

--- src/main/java/com/green/day07/ch11/MissionExt.java
+++ src/main/java/com/green/day07/ch11/MissionExt.java
@@ -2,7 +2,7 @@ package com.green.day07.ch11;
 
 public class MissionExt {
     public static void main(String[] args) {
-        //리터럴 사용 금지
+        //점의 위치값으로 리터럴 사용 금지
         //fileNm에서 jpg문자열을 뽑아내주세요.
         String fileNm = "nice_v.ery.Nice.jpg";
 
