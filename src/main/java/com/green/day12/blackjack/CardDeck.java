7723025232d204ff8e202e5d577aaac8f7e89a98
DoheumPark <pirbak@hanmail.net>
2024-10-23 17:10:51 +0900

    10-23 (오후)(2)

--- src/main/java/com/green/day12/blackjack/CardDeck.java
+++ src/main/java/com/green/day12/blackjack/CardDeck.java
@@ -4,6 +4,7 @@ public class CardDeck {
     //Card 객체 주소값 52개를 저장할 수 있어야 한다.
     private final Card[] cards;
     private final String[] patterns;
+    private int selectedIdx = 0; //외부에 줘야할 카드의 index값
 
     public CardDeck() {
         cards = new Card[52];
@@ -12,10 +13,48 @@ public class CardDeck {
         //미션1
         // "", "" 값을 가지고 있는 Card객체를 52개를 만들어주시고
         // cards배열에 하나씩 넣어주세요.
-        for(int i=0; i<cards.length; i++) {
-            cards[i] = new Card("A", "B"); //A(B)
+
+
+        //미션2
+        //모두 다른값을 가지고 있는 Card객체 만들어서 cards에 순차적으로 넣어주세요.
+        int idx = 0;
+        for(int i=0; i<patterns.length; i++) { //0, 1, 2, 3
+            String pattern = patterns[i];
+            for(int z=1; z<=13; z++) { // 1~13
+                String denomination = getDenomination(z);
+                cards[idx++] = new Card(pattern, denomination);
+
+                //cards[idx++] = new Card(patterns[i], getDenomination(z));
+            }
         }
 
+        //섞어주세요. (랜덤, 스와핑)
+        shuffle();
+
+        //enhanced-for문으로 card객체 주소값 전부 출력
+        for(Card c : cards) {
+            System.out.println(c);
+        }
+    }
+
+    private void shuffle() {
+        for(int i=0; i<cards.length; i++) {
+            int rIdx = (int)(Math.random() * cards.length); //0 ~ 51
+            Card tmp = cards[i];
+            cards[i] = cards[rIdx];
+            cards[rIdx] = tmp;
+        }
+    }
+
+    //주소값 하나씩 리턴, 리턴 주소값 지워야되고, selectedIdx 이용하여 해결
+    public Card draw() {
+        Card c = cards[selectedIdx];
+        cards[selectedIdx] = null;
+        selectedIdx++;
+        return c;
+    }
+
+    public void checkCard() {
         //enhanced-for문으로 card객체 주소값 전부 출력
         for(Card c : cards) {
             System.out.println(c);
@@ -34,4 +73,7 @@ public class CardDeck {
             default: return String.valueOf(n);
         }
     }
+
+
+
 }
--- src/main/java/com/green/day12/blackjack/CardTest.java
+++ src/main/java/com/green/day12/blackjack/CardTest.java
@@ -2,14 +2,10 @@ package com.green.day12.blackjack;
 
 public class CardTest {
     public static void main(String[] args) {
-        CardDeck cardDeck = new CardDeck();
-        System.out.println(cardDeck.getDenomination(1)); //"A"
-        System.out.println(cardDeck.getDenomination(2)); //"2"
-        System.out.println(cardDeck.getDenomination(3)); //"3"
-        System.out.println(cardDeck.getDenomination(10)); //"10"
-        System.out.println(cardDeck.getDenomination(11)); //"J"
-        System.out.println(cardDeck.getDenomination(12)); //"Q"
-        System.out.println(cardDeck.getDenomination(13)); //"K"
-        System.out.println(cardDeck.getDenomination(14)); //""
+        CardDeck cd = new CardDeck();
+
+        Card c = cd.draw();
+        Card c2 = cd.draw();
+        cd.checkCard();
     }
 }
